a2fb945331e67f2461313b3561bd44db2d6f1e41
Gaurav singh <golurawat151106@gmail.com>
2026-03-15 09:52:04 +0530

    fix the structure

--- controller/url.js
+++ controller/url.js
@@ -16,7 +16,24 @@ return res.status(500).json({error: err.message})
 }
 }
 
+async function geturl(req,res){
+    try {
+            const id = req.params.id;
+            if (!id) return res.status(400).json({ error: "id is not available" });
+            
+            const urldata = await url.findOne({ shortid: id });
+            if (!urldata) return res.status(404).json({ error: "url not found" });
+            
+            await url.findOneAndUpdate({ shortid: id }, { totalclicks: urldata.totalclicks + 1 });
+            return res.redirect(urldata.redirecturl);
+        } catch(err) {
+            return res.status(500).json({ error: err.message });
+        }
+    }
+
 
 module.exports={
     shoturl,
+    geturl,
+    
 }
--- routes/url.js
+++ routes/url.js
@@ -1,21 +1,9 @@
 const express = require("express")
-const { shoturl } = require('../controller/url')
+const { shoturl,geturl } = require('../controller/url')
 const url = require("../models/url")
 const router = express.Router()
 
 router.post('/', shoturl)
-router.get('/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        if (!id) return res.status(400).json({ error: "id is not available" });
-        
-        const urldata = await url.findOne({ shortid: id });
-        if (!urldata) return res.status(404).json({ error: "url not found" });
-        
-        await url.findOneAndUpdate({ shortid: id }, { totalclicks: urldata.totalclicks + 1 });
-        return res.redirect(urldata.redirecturl);
-    } catch(err) {
-        return res.status(500).json({ error: err.message });
-    }
-})
+router.get('/:id', geturl)
+    
 module.exports = router;
